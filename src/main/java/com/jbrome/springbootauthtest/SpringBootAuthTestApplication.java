a64cf11087e8b24ff9a6a99e7433aad7a7d1b69c
Jinbeom Kim <jbkim@kaltour.com>
2020-01-16 11:33:12 +0900

    init

---
+++ src/main/java/com/jbrome/springbootauthtest/SpringBootAuthTestApplication.java
@@ -0,0 +1,13 @@
+package com.jbrome.springbootauthtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAuthTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAuthTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jbrome/springbootauthtest/SpringBootAuthTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbrome.springbootauthtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAuthTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
